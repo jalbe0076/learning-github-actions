6d288cd10b0f685ba706527cca1b597f04a47d93
Jason Alberto <alberto.jason@gmail.com>
2024-09-29 18:24:05 -0400

    Update app using with for resource allocation

--- Ch04/04_02/app.py
+++ Ch04/04_02/app.py
@@ -8,7 +8,8 @@ from flask import Flask, jsonify, abort, make_response
 APP = Flask(__name__)
 
 # Load the data
-MASCOTS = json.load(open('data.json', 'r'))
+with open('data.json', 'r', encoding='utf-8') as file:
+    MASCOTS = json.load(file)
 
 
 @APP.route('/', methods=['GET'])
